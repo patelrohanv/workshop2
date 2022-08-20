b714848c03503b02a5e09eec173e799f2a2871bf
Rohan Patel <patel@Rohans-Air.attlocal.net>
2022-08-20 12:03:47 -0500

    setting clean template

--- app/nc_tutorials/urls.py
+++ app/nc_tutorials/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+# ^ TASK 4 import static and settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tutorials.urls')),
     path('', include('users.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 # ^ TASK 4 append static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) to urlpatterns
